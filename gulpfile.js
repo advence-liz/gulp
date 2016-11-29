a50739a7cf446d699861b2bdf3f755b0e941b93c
advence-liz <1012515183@qq.com>
2016-11-29 11:31:18 +0800

    合并压缩 synatxhighlighter
    /test/syntaxhightlihter.js  压缩版
    /test/syntax.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require("gulp"), //本地安装gulp所用到的地方
     minifycss = require("gulp-minify-css"),
     rename = require("gulp-rename"),
     webpack = require("webpack-stream"),
+    concat = require("gulp-concat"),
+    uglify = require('gulp-uglify'),
     babel = require("gulp-babel");
 //定义一个testLess任务（自定义任务名称）
 gulp.task("testLess", function () {
@@ -29,22 +31,33 @@ gulp.task("testbabel", function () {
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("syntax:js", function () {
-  return  gulp.src("syntaxhighlighter/build/pack.js")
-        .pipe(webpack({
-            output: {
-                filename: "syntax.js"
-            }
-        }))
-        .pipe(gulp.dest("syntaxhighlighter/dist"));
-});
+// gulp.task("syntax:js", function () {
+//   return  gulp.src("syntaxhighlighter/build/pack.js")
+//         .pipe(webpack({
+//             output: {
+//                 filename: "syntax.js"
+//             }
+//         }))
+//         .pipe(gulp.dest("syntaxhighlighter/dist"));
+// });
 gulp.task("syntax:less", function () {
-    return gulp.src("syntaxhighlighter/build/pack.less") 
-        .pipe(less()) 
+    return gulp.src("syntaxhighlighter/build/pack.less")
+        .pipe(less())
         .pipe(rename("syntax.css"))
-        .pipe(gulp.dest("syntaxhighlighter/dist")) 
+        .pipe(gulp.dest("syntaxhighlighter/dist"))
 });
 
+gulp.task("ss", function () {
+    return gulp.src(['syntaxhighlighter/scripts/shCore.js',
+        'syntaxhighlighter/scripts/shBrushCss.js',
+        'syntaxhighlighter/scripts/shBrushJScript.js',
+        'syntaxhighlighter/scripts/shBrushXml.js','syntaxhighlighter/scripts/init.js'])
+        .pipe(concat({path: 'synatx.js'}))
+        .pipe(gulp.dest('./synatx/'))
+        .pipe(rename("synatx.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest('./synatx/'))
+})
 gulp.task("default", ["testLess", "minifycss"]); //定义默认任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
