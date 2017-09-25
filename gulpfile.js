ef9e738c10762b95623e08d6cf577f778e8a84eb
advence-liz <1012515183@qq.com>
2017-09-25 13:07:05 +0800

    update current master then try use  git branch organize solution

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 //导入工具包 require('node_modules里对应模块')
+"use strict";
 var gulp = require("gulp"), //本地安装gulp所用到的地方
     clean = require("gulp-clean"),
-    // less = require("gulp-less"),
     less = require('gulp-less-sourcemap'),
     rename = require("gulp-rename"),
-    // webpack = require("webpack-stream"),
-    through2 = require("through2"),
+    git = require('gulp-git'),
     concat = require("gulp-concat"),
     uglify = require('gulp-uglify'),
-    babel = require("gulp-babel");
+    babel = require("gulp-babel"),
+    Q = require("q");
 
 gulp.task("clean", function () {
     return gulp.src("dist/", { read: false }).pipe(clean());
@@ -25,12 +25,12 @@ gulp.task("testbabel", function () {
 
 gulp.task("less:source", function () {
     gulp.src("src/less/index.less")
-    .pipe(less({
-        sourceMap: {
-            sourceMapRootpath: '../../src/less/' // Optional absolute or relative path to your LESS files// dir relative of dest
-        }
-    }))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(less({
+            sourceMap: {
+                sourceMapRootpath: '../../src/less/' // Optional absolute or relative path to your LESS files// dir relative of dest
+            }
+        }))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task("syntax:less", function () {
@@ -46,38 +46,14 @@ gulp.task("ss", function () {
         'syntaxhighlighter/scripts/shBrushJScript.js',
         'syntaxhighlighter/scripts/shBrushXml.js',
         'syntaxhighlighter/scripts/init.js'])
-        .pipe(concat({path: 'syntax.js'}))
+        .pipe(concat({ path: 'syntax.js' }))
         .pipe(gulp.dest('./syntax/'))
         .pipe(rename("syntax.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest('./syntax/'))
 })
-// function modify(modifier) {
-//     return through2.obj(function(file, encoding, done) {
-//         var content = modifier(String(file.contents));
-//         file.contents = new Buffer(content);
-//         this.push(file);
-//         done();
-//     });
-// }
-// gulp.task('modify', function() {
-//     return gulp.src('app.js')
-//         .pipe(modify(version))
-//         .pipe(modify(swapStuff))
-//         .pipe(gulp.dest('build'));
-// });
-// function version(data) {
-//     return data.replace(/__VERSION__/, pkg.version);
-// }
-// function swapStuff(data) {
-//     return data.replace(/(\w+)\s(\w+)/, '$2, $1');
-// }
 
 
-gulp.task("test", function () {
-
-   console.log("test");
-});
 //gulp.task("default", ["testLess", "minifycss"]); //定义默认任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
