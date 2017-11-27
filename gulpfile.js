00e5a87a183f64a83533ff307211fbfa29a77e88
advence-liz <1012515183@qq.com>
2017-11-27 17:29:56 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 //导入工具包 require('node_modules里对应模块')
 "use strict";
 const gulp = require("gulp"); //本地安装gulp所用到的地方
-
-
 // through2 是一个对 node 的 transform streams 简单封装
+const path = require("path");
 var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
@@ -44,10 +43,20 @@ function gulpPrefixer(prefixText) {
 
 }
 
-// 暴露（export）插件主函数
-//module.exports = gulpPrefixer;
+gulp.task("pre",function(){
+  gulp.src("src/index.js")
+  .pipe(gulpPrefixer("liz"))
+  .pipe(gulp.dest("dist"))
+})
 
 
+gulp.task("pre",function(){
+ 
+ return gulp.src(path.join('src','index.js')) 
+  .pipe(gulpPrefixer("liz"))
+  .pipe(gulp.dest("dist"))
+})
+gulp.start("pre");
 //gulp.task("default", ["testLess", "minifycss"]); //定义默认任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
